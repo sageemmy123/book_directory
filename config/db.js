e5c1f6ca57e978dd52520577af9095f47b6bac91
Sageemmy123 <Sage.emmy123@gmail.com>
2023-01-05 10:44:09 +0300

    first commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require ('mongoose');
-//var url = 'mongodb://localhost:27017/booksDB';
-var url = 'mongodb+srv://sage1234:Sports123@mernappcluster.h7jvjn8.mongodb.net/mernapp';
+var url = 'mongodb://localhost:27017/booksDB';
 
 const connection = mongoose.createConnection(url);
 
